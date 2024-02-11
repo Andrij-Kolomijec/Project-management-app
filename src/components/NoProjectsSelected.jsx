e4e038187c8ced09193ca63984d23631b22c1867
Difficins <a.kolomijec@seznam.cz>
2024-02-11 09:51:19 +0100

    Minor adjustments

--- src/components/NoProjectsSelected.jsx
+++ src/components/NoProjectsSelected.jsx
@@ -3,17 +3,17 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 import noProjects from "../assets/no-projects.png";
 
-export default function NoProjectsSelected({ onAddClick }) {
+export default function NoProjectsSelected({ onStartAddProject }) {
   return (
     <section className={classes.details}>
       <img src={noProjects} alt="Empty Clipboard" />
       <h2>No project selected</h2>
       <p>Select a project or get started with a new one.</p>
-      <Button onClick={() => onAddClick("input")}>Create new project</Button>
+      <Button onClick={onStartAddProject}>Create new project</Button>
     </section>
   );
 }
 
 NoProjectsSelected.propTypes = {
-  onAddClick: PropTypes.func,
+  onStartAddProject: PropTypes.func,
 };
